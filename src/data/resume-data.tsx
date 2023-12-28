b753a3a16bf2a837c06a09152c469214dd4471e9
Mobiuus <100689677+Mobiuus@users.noreply.github.com>
2023-12-29 00:05:01 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -32,7 +32,7 @@ export const RESUME_DATA = {
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
     email: "valentin.courtaud@skema.edu",
-    tel: "+330695308946"
+    tel: "+330695308946",
     social: [
       {
         name: "LinkedIn",
